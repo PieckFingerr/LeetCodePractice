ee6532ba88f39d2d7bcf09f2c6affe9fadd743d0
PieckFingerr <vinhgiachau@gmail.com>
2024-03-14 11:51:12 +0700

    Add file

---
+++ Arrays_str/_724_Pivot_Idex.java
@@ -0,0 +1,25 @@
+package Arrays_str;
+
+public class _724_Pivot_Idex {
+    
+    public static int pivotIndex(int[] a){
+        int n = a.length;
+        int L = 0, R = 0;
+        for (int i : a){
+            R += i;
+        }
+        for (int i = 0; i < n; i++){
+            R -= a[i];
+            if ( R == L ){
+                return i;
+            }
+            L += a[i];
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {2, 1, -1};
+        System.out.println(pivotIndex(a));
+    }
+}
